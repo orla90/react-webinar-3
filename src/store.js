3a977df24464a497f2c88cdd5a74d8e8a5267115
Olga Andrievich <olgakuz2302@gmail.com>
2023-05-15 11:41:50 +0300

    refactor code

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {} ) {
+  constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
     this.count = 1;
@@ -74,42 +74,21 @@ class Store {
    * Выделение записи по коду
    * @param code
    */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        item.selected = (item.code === code) ? !item.selected : false;
-        if (item.code === code && item.selectedCount && item.selected) {
-          item.selectedCount = item.selectedCount + 1;
-          item.title = this.editTitle(item);
-        } else if (item.code === code && !item.selectedCount && item.selected) {
-          item.selectedCount = 1;
-          item.title = this.editTitle(item);
-        } 
-        return item;
+  selectItem(e, code) {
+    if (e.target.tagName.toLowerCase() !== 'button') {
+      this.setState({
+        ...this.state,
+        list: this.state.list.map(item => {
+          item.selected = (item.code === code) ? !item.selected : false;
+          if (item.code === code && item.selectedCount && item.selected) {
+            item.selectedCount = item.selectedCount + 1;
+          } else if (item.code === code && !item.selectedCount && item.selected) {
+            item.selectedCount = 1;
+          }
+          return item;
+        })
       })
-    })
-  }
-  
-  /**
-   * Корректировка заголовка записи
-   * @param item {Object}
-   * @returns {string}
-   */
-  editTitle(item) {
-    return `${item.title.split(' | ')[0]} | Выделяли ${item.selectedCount} ${this.checkWordDeclension(item.selectedCount, ['раз', 'раза', 'раз'])}`
-  }
-  
-  /**
-   * Функция для склонения слов
-   * @param number {number} 
-   * @param words {[string]}
-   * @returns {string}
-   */
-  checkWordDeclension(number, words) {  
-    return words[(number % 100 > 4 && number % 100 < 20) ? 2
-      : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10
-        : 5]];
+    }
   }
 }
 
